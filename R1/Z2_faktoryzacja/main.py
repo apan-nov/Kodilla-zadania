ccd2def1a7cbee9e7efe3f6d0f58c243045f5b52
Adrian Pankiewicz <apankiewicz@novomatic-tech.com>
2024-01-27 23:34:16 +0100

    adding main file for Z2 project

---
+++ R1/Z2_faktoryzacja/main.py
@@ -0,0 +1,15 @@
+# Rozkład liczby całkowitej na czynniki pierwsze jest algorytmem istotnym z punktu widzenia kryptografii.
+# Stosując technikę TDD, zaimplementuj funkcję prime_factors(number),
+# zwracającą czynniki pierwsze number w postaci listy.
+# Jeśli number jest liczbą pierwszą, lista powinna zawierać wyłącznie ją samą.
+# Przykładowo: prime_factors(3958159172) powinno zwrócić [2, 2, 11, 2347, 38329].
+#
+# Liczy się proces dochodzenia do rozwiązania (udokumentowany historią repozytorium),
+# a nie samo rozwiązanie. Nie zapomnij o uwzględnieniu przypadków szczególnych.
+
+
+def prime_factors(number):
+	number = input("Podaj liczbę: ")
+	print(number)
+
+print("test")
